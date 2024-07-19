c8bbaf20959294f6bc8775de05bd8ab856c121ce
Joao Vicente <joao.diogo.vicente@gmail.com>
2024-07-19 13:35:11 +0100

    Making pytest run from CLI

--- test/repositories/open_position_repository_test.py
+++ test/repositories/open_position_repository_test.py
@@ -1,9 +1,11 @@
 import os
 import datetime
+from test.conftest import abspath
+
 from runtime.api.repositories.open_position_repository import OpenPositionRepository
 
 def test_open_position_repository():
-    path = os.path.join("test","data", "open_position.csv")
+    path = os.path.join(abspath, "data", "open_position.csv")
     repo = OpenPositionRepository(path=path)
     positions = repo.get_all()
     assert positions[0].date == datetime.date(2024, 6, 18)
