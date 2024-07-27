be9d1df075ce6ba26c594a75de2b8bfea69f55b7
Joao Vicente <joao.diogo.vicente@gmail.com>
2024-07-27 08:59:03 +0100

    brought notebooks to project root

--- notebooks/conf.py
+++ notebooks/conf.py
@@ -1,5 +1,5 @@
 
 import sys, os
 current_dir = os.getcwd()
-src_path = os.path.join(current_dir, '..')
+src_path = os.path.join(current_dir, '..', 'src')
 sys.path.append(os.path.abspath(src_path))
