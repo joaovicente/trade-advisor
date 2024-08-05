8b0dc2c75c256e68d49975509787573d69552906
Joao Vicente <joao.diogo.vicente@gmail.com>
2024-08-05 18:17:24 +0100

    Prepared code to optimise alternative strategy

--- src/services/backtesting_service.py
+++ src/services/backtesting_service.py
@@ -10,6 +10,24 @@ class BaseBacktraderStrategy(bt.Strategy):
     INDICATOR_WARMUP_IN_WEEKS = 24
     INDICATOR_WARMUP_IN_DAYS = INDICATOR_WARMUP_IN_WEEKS * 5 # 5 market-active days per week
     TRADE_ACTION_CONTEXT_SIZE = 5
+    def __init__(self):
+        super().__init__() 
+        if self.params.single_date_to_trade is not None:
+            self.single_date_to_trade = datetime.datetime.strptime(self.params.single_date_to_trade, "%Y-%m-%d").date()
+        else:
+            self.single_date_to_trade = None    
+        # The attributes below are stored as dictionaries keyed by ticker name (e.g. AMZN)
+        self.order = {data._name: None for data in self.datas}
+        self.buyprice = {data._name: None for data in self.datas}
+        self.buycomm = {data._name: None for data in self.datas}
+        self.last_bought_order_date = {data._name: None for data in self.datas}
+        self.stock_daily_stats_list = {data._name: [] for data in self.datas}            
+        # Add the RSI indicator
+        self.rsi = {data._name: bt.indicators.RSI(data,plot=True) for data in self.datas}
+        self.rsi_ma = {data._name: bt.indicators.SmoothedMovingAverage(self.rsi[data._name], period=14,plot=True) for data in self.datas}
+        # Trade action list
+        self.trade_actions = []
+        
     def log(self, txt, dt=None, do_print=False):
         ''' Logging function for strategy'''
         if self.params.printlog or do_print:
@@ -69,67 +87,6 @@ class BaseBacktraderStrategy(bt.Strategy):
     def days_in_buffer(self):
         return len(self)
     
-class RsiBollingerStrategy(BaseBacktraderStrategy):
-    def __init__(self):
-        super().__init__() 
-    # TODO: Implement strategy
-    # Buy when lower bb is crossed and rsi below 40
-    # Sell when median bb falls below its recent peak, at a magnitude of n multiplier of ATR (optimise using different multiplier values on stocks that are not so bullish)
-    # References:
-    #- https://blackwellglobal.com/the-bollinger-bands-rsi-trading-strategy/
-    #- https://github.com/Worlddatascience/DataScienceCohort/blob/refs/heads/master/8_How_to_Backtest_a_Bollinger_Bands_Strategy.ipynb
-
-class BacktraderStrategy(BaseBacktraderStrategy):
-    params = (
-        ('start_date', None),
-        ('printlog', False),
-        ('upper_rsi', 60),
-        ('lower_rsi', 50),
-        ('loss_pct_threshold', 5),
-        ('profit_protection_pct_threshold', 0), # 0 = allow profit to come down to 0%
-        ('fixed_investment_amount', 3000),
-        ('single_date_to_trade', None), # date string expected (e.g. 2023-12-31)
-        ('custom_callback', None),
-        ('open_positions', None)
-    )
-
-    def __init__(self):
-        super().__init__() 
-        if self.params.single_date_to_trade is not None:
-            self.single_date_to_trade = datetime.datetime.strptime(self.params.single_date_to_trade, "%Y-%m-%d").date()
-        else:
-            self.single_date_to_trade = None
-        
-        # The attributes below are stored as dictionaries keyed by ticker name (e.g. AMZN)
-        self.order = {data._name: None for data in self.datas}
-        self.buyprice = {data._name: None for data in self.datas}
-        self.buycomm = {data._name: None for data in self.datas}
-        self.last_bought_order_date = {data._name: None for data in self.datas}
-        self.stock_daily_stats_list = {data._name: [] for data in self.datas}
-            
-        # Add the RSI indicator
-        self.rsi = {data._name: bt.indicators.RSI(data,plot=True) for data in self.datas}
-        self.rsi_ma = {data._name: bt.indicators.SmoothedMovingAverage(self.rsi[data._name], period=14,plot=True) for data in self.datas}
-        
-        # Trade action list
-        self.trade_actions = []
-        
-
-    def buy_action(self, name):
-        buy_action = None
-        # Buy when RSI goes over RSI-MA while under lower_rsi
-        rsi_below_lower_threshold = self.rsi[name][0] < self.params.lower_rsi
-        rsi_crossed_above_rsi_ma = self.rsi[name][-1] < self.rsi_ma[name][-1] and self.rsi[name][0] > self.rsi_ma[name][0]
-        is_todays_date = self.single_date_to_trade == self.datas[0].datetime.date(0)
-        if not self.trade_today_mode():
-            buy = rsi_below_lower_threshold and rsi_crossed_above_rsi_ma
-        else:
-            buy = rsi_below_lower_threshold and rsi_crossed_above_rsi_ma and is_todays_date
-        if buy:
-            buy_action = TradeAction(date=self.datas[0].datetime.date(0), action="BUY", ticker=name)
-            buy_action.reason = f"{name} RSI: {self.rsi[name][0]:.2f} (yesterday={self.rsi[name][-1]:.2f}) above RSI-MA {self.rsi_ma[name][0]:.2f} under RSI < {self.params.lower_rsi:.2f} threshold"
-        return buy_action
-       
     def buy_position_recorded(self, name):
         open_position_recorded = None
         if self.params.open_positions is not None:
@@ -153,42 +110,6 @@ class BacktraderStrategy(BaseBacktraderStrategy):
             peak_price = max(peak_price, data.close[k])
         return peak_price
     
-    def sell_action(self, name):
-        sell_action = TradeAction(date=self.datas[0].datetime.date(0), action="SELL", ticker=name)
-        # Sell when RSI crosses over RSI-based-MA coming down above RSI 60, or when position showing 10% loss
-        data = self.getdatabyname(name)
-        reached_maximum_tolerated_loss = False
-        reached_maximum_profit_loss_tolerance = False
-        # Nothing to sell or not current-trade-day
-        if not self.getposition(data) or \
-            (self.trade_today_mode() and self.single_date_to_trade != self.datas[0].datetime.date(0)):
-            return False
-        else:
-            # Profitable position
-            if data.close[0] > self.getposition(data).price:
-                # Check if we want to protect profit when price is comming down
-                # profit_protection_pct_threshold (0 = all loss of all profit made)
-                share_price_peak = self.peak_price_since_bought(data) 
-                peak_share_profit = share_price_peak - self.getposition(data).price
-                current_per_share_profit = data.close[0] - self.getposition(data).price 
-                profit_pct =  current_per_share_profit / peak_share_profit * 100
-                if profit_pct < self.params.profit_protection_pct_threshold:
-                    reached_maximum_profit_loss_tolerance = True
-                    sell_action.reason = f"{name} Maximum profit loss tolerance reached {self.params.profit_protection_pct_threshold}% Selling with {profit_pct:.2f}% profit"
-                    self.log(sell_action.reason)
-            else:
-                # Check if maximum tolerated loss
-                percent = self.params.loss_pct_threshold
-                pnl_perc = 1 - (self.getposition(data).price / data.close[0])
-                if pnl_perc < -1 * (percent/100):
-                    sell_action.reason = f"{name} Maximum tolerated loss reached {percent:.2f}% Selling with {pnl_perc*100*-1:.2f}% loss"
-                    self.log(sell_action.reason)
-                    reached_maximum_tolerated_loss = True
-        if reached_maximum_profit_loss_tolerance or reached_maximum_tolerated_loss:
-            return sell_action
-        else:
-            return None
-   
     def next(self):
         for data in self.datas:
             # Warm-up RSI for rsi_warmup_in_days
@@ -239,9 +160,89 @@ class BacktraderStrategy(BaseBacktraderStrategy):
                     self.order[data._name] = self.sell(data = data, size = self.getposition(data).size)
                     sell_action.context = [s.as_text() for s in self.stock_daily_stats_list[data._name][-BacktraderStrategy.TRADE_ACTION_CONTEXT_SIZE:]]
                     self.trade_actions.append(sell_action)
+class RsiBollingerStrategy(BaseBacktraderStrategy):
+    def __init__(self):
+        super().__init__() 
+    # TODO: Implement strategy
+    # Buy when lower bb is crossed and rsi below 40
+    # Sell when median bb falls below its recent peak, at a magnitude of n multiplier of ATR (optimise using different multiplier values on stocks that are not so bullish)
+    # References:
+    #- https://blackwellglobal.com/the-bollinger-bands-rsi-trading-strategy/
+    #- https://github.com/Worlddatascience/DataScienceCohort/blob/refs/heads/master/8_How_to_Backtest_a_Bollinger_Bands_Strategy.ipynb
+
+class BacktraderStrategy(BaseBacktraderStrategy):
+    params = (
+        ('start_date', None),
+        ('printlog', False),
+        ('upper_rsi', 60),
+        ('lower_rsi', 50),
+        ('loss_pct_threshold', 5),
+        ('profit_protection_pct_threshold', 0), # 0 = allow profit to come down to 0%
+        ('fixed_investment_amount', 3000),
+        ('single_date_to_trade', None), # date string expected (e.g. 2023-12-31)
+        ('custom_callback', None),
+        ('open_positions', None)
+    )
+
+    def __init__(self):
+        super().__init__() 
+        
+
+    def buy_action(self, name):
+        buy_action = None
+        # Buy when RSI goes over RSI-MA while under lower_rsi
+        rsi_below_lower_threshold = self.rsi[name][0] < self.params.lower_rsi
+        rsi_crossed_above_rsi_ma = self.rsi[name][-1] < self.rsi_ma[name][-1] and self.rsi[name][0] > self.rsi_ma[name][0]
+        is_todays_date = self.single_date_to_trade == self.datas[0].datetime.date(0)
+        if not self.trade_today_mode():
+            buy = rsi_below_lower_threshold and rsi_crossed_above_rsi_ma
+        else:
+            buy = rsi_below_lower_threshold and rsi_crossed_above_rsi_ma and is_todays_date
+        if buy:
+            buy_action = TradeAction(date=self.datas[0].datetime.date(0), action="BUY", ticker=name)
+            buy_action.reason = f"{name} RSI: {self.rsi[name][0]:.2f} (yesterday={self.rsi[name][-1]:.2f}) above RSI-MA {self.rsi_ma[name][0]:.2f} under RSI < {self.params.lower_rsi:.2f} threshold"
+        return buy_action
+       
+    def sell_action(self, name):
+        sell_action = TradeAction(date=self.datas[0].datetime.date(0), action="SELL", ticker=name)
+        # Sell when RSI crosses over RSI-based-MA coming down above RSI 60, or when position showing 10% loss
+        data = self.getdatabyname(name)
+        reached_maximum_tolerated_loss = False
+        reached_maximum_profit_loss_tolerance = False
+        # Nothing to sell or not current-trade-day
+        if not self.getposition(data) or \
+            (self.trade_today_mode() and self.single_date_to_trade != self.datas[0].datetime.date(0)):
+            return False
+        else:
+            # Profitable position
+            if data.close[0] > self.getposition(data).price:
+                # Check if we want to protect profit when price is comming down
+                # profit_protection_pct_threshold (0 = all loss of all profit made)
+                share_price_peak = self.peak_price_since_bought(data) 
+                peak_share_profit = share_price_peak - self.getposition(data).price
+                current_per_share_profit = data.close[0] - self.getposition(data).price 
+                profit_pct =  current_per_share_profit / peak_share_profit * 100
+                if profit_pct < self.params.profit_protection_pct_threshold:
+                    reached_maximum_profit_loss_tolerance = True
+                    sell_action.reason = f"{name} Maximum profit loss tolerance reached {self.params.profit_protection_pct_threshold}% Selling with {profit_pct:.2f}% profit"
+                    self.log(sell_action.reason)
+            else:
+                # Check if maximum tolerated loss
+                percent = self.params.loss_pct_threshold
+                pnl_perc = 1 - (self.getposition(data).price / data.close[0])
+                if pnl_perc < -1 * (percent/100):
+                    sell_action.reason = f"{name} Maximum tolerated loss reached {percent:.2f}% Selling with {pnl_perc*100*-1:.2f}% loss"
+                    self.log(sell_action.reason)
+                    reached_maximum_tolerated_loss = True
+        if reached_maximum_profit_loss_tolerance or reached_maximum_tolerated_loss:
+            return sell_action
+        else:
+            return None
+   
 
     def stop(self):
         if not self.trade_today_mode():
+            #TODO: Make log statment more generic and push to parent class
             self.log(f'RSI: {self.params.upper_rsi}/{self.params.lower_rsi}, ' +
                     f'loss_pct: {self.params.loss_pct_threshold}, '
                     f'investment: {self.params.fixed_investment_amount}, '
