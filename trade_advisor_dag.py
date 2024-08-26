34b32155cdaeb7b158a4f2c27bff2a227799e07d
Joao Vicente <joao.diogo.vicente@gmail.com>
2024-08-26 19:36:25 +0100

    added email sender variables

--- trade_advisor_dag.py
+++ trade_advisor_dag.py
@@ -25,8 +25,11 @@ def trade_advisor_dag():
         environment = {
             "TWILIO_ACCOUNT_SID": '{{var.value.twilio_account_sid}}',
             "TWILIO_AUTH_TOKEN": '{{var.value.twilio_auth_token}}',
-            "WHATSAPP_SENDER_NUMBER": '{{var.value.whatsapp_sender_number}}',
-            "WHATSAPP_RECEIVER_NUMBER": '{{var.value.whatsapp_receiver_number}}'
+            "WHATSAPP_SENDER_NUMBER": '{{var.value.trade_advisor_whatsapp_sender_number}}',
+            "WHATSAPP_RECEIVER_NUMBER": '{{var.value.trade_advisor_whatsapp_receiver_number}}',
+            "SENDGRID_API_KEY": '{{var.value.sendgrid_api_key}}',
+            "EMAIL_SENDER": '{{var.value.trade_advisor_email_sender}}',
+            "EMAIL_RECEIVER": '{{var.value.trade_advisor_email_receiver}}'
         },
         command="/app/src/cli/cli.py trade-today --output=whatsapp --tickers AAPL,ABBV,ADBE,AMD,AMZN,AVGO,BAC,BRK-B,COST,CRM,CVX,GOOG,HD,JNJ,JPM,KO,LLY,MA,META,MRK,MSFT,NFLX,NVDA,ORCL,PEP,PFE,PG,TMO,TSLA,UNH,V,WMT,XOM -p 2024-07-18,META,2.09692,476.89 -p 2024-07-18,GOOG,5.56917,179.56 -p 2024-07-18,NVDA,8.28706,120.67 -p 2024-07-18,AMZN,2.68962,185.90 -p 2024-07-18,MSFT,0.5328,438.16"
     )
